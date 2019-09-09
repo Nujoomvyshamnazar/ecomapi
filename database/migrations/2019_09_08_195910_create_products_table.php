7ed43c516079dbb8480997f0c0e64f9356b62c24
nujoom nazar <nujoomvyshamnazar@gmail.com>
2019-09-09 13:36:08 +0530

    migration created for product and review

--- database/migrations/2019_09_08_195910_create_products_table.php
+++ database/migrations/2019_09_08_195910_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
